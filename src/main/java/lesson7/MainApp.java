11252a64fbbea43b0a32b50e2f66be8f86751262
Павел <pavel.ulkin@gmail.com>
2021-02-03 21:30:30 +0300

    task7

--- src/main/java/lesson7/MainApp.java
+++ src/main/java/lesson7/MainApp.java
@@ -6,7 +6,7 @@ public class MainApp {
 
         Cat[] cats = {new Cat("Филя", 14), new Cat("Маркел", 4),
                 new Cat("Босс", 5), new Cat("Граф", 6),
-                new Cat("Барс", 7)};
+                new Cat("Барс", 7), new Cat("Обжора", 20)};
 
         plate.fillPlate(40);
         for (Cat cat : cats) {
