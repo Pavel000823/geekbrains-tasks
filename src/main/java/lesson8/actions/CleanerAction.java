01566b7d35a1cfcccf0c1d006ffb4a64e9444048
Павел <pavel.ulkin@gmail.com>
2021-02-08 13:07:20 +0300

    task8

---
+++ src/main/java/lesson8/actions/CleanerAction.java
@@ -0,0 +1,20 @@
+package lesson8.actions;
+
+import lesson8.Chat;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class CleanerAction implements ActionListener {
+
+    private final Chat chat;
+
+    public CleanerAction(Chat chat) {
+        this.chat = chat;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        chat.getInputField().setText("");
+    }
+}
